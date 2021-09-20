07a1fa7992f640f1e8c634944ee3ad42b964d8a0
swells25 <89562708+swells25@users.noreply.github.com>
2021-09-19 18:57:42 -0600

    Hello & calculator Microsoft

---
+++ calc.py
@@ -0,0 +1,22 @@
+python
+C:\Users\Stacy>python
+Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] :: Anaconda, Inc. on win32
+
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+print("The sum is: ", first_number + second_number)
+
+... second_number = int(input('Type the second number: ')) ;\
+... print("The sum is: ", first_number + second_number)
+Type the first number:
+
+>>> first_number = int(input('Type the first number: ')) ;\
+... second_number = int(input('Type the second number: ')) ;\
+... print("The sum is: ", first_number + second_number)
+Type the first number: 2
+Type the second number: 3
+The sum is:  5
+>>>
+
+
+
---
+++ hello.py
@@ -0,0 +1,38 @@
+x = 1         # assign variable x the value 1
+y = x + 5     # assign variable y the value of x plus 5
+z = y         # assign variable z the value of y
+
+x = 1    #integer
+x = 1.0  # decimal (floating point)    
+
+x = 1
+print(type(x)) # outputs: <class 'int'>
+x = 1.0 
+pring(type(x)) # outputs: <class 'float'>
+
+x = True
+print(type(x)) # outputs: <class 'bool'>
+
+x = 'This is a string'
+print(x) # outputs: This is a string 
+print(type(x)) # outputs: <class 'str'>
+y = "This is alos a string"
+
+x = 'Hello' + ' '+'World:'
+print(x) # outputs: Hellow World! 
+
+print('Hello World!')
+
+>>> PI = 3.14
+>>> PI
+3.14
+
+name = input('Stacy:')
+print(name)
+
+x = int(input('Enter a number: '))
+print(type(5))
+
+x= 5 
+print('The number is ' + str(x))
+The number is 5 
